c23fc7aeb8bbbc6433e6883d33561d34fd4776b6
@leoachear <leoachear@gmail.com>
2020-05-17 11:54:15 -0300

    Pruebas con el CLI de Vue.js

---
+++ src/components/Lista.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <li v-for="(item, index) of arrayOrdenado" :key="item.id" @click="aumentar(index)"
+            class="list-group-item d-flex justify-content-between align-items-center">
+                {{index}} - {{item.nombre}}
+                <span class="badge badge-primary badge-pill">{{item.cantidad}}</span>
+            </li>
+        </ul>
+        <button class="btn btn-danger btn-block" @click="reinicar">Reinicar</button>
+    </div>
+</template>
+
+<script>
+
+    import {mapState, mapMutations} from 'vuex';
+
+    export default {
+        name: 'Lista',
+        computed:{
+            ...mapState(['frutas']),
+            arrayOrdenado(){
+                let frutasCopia = this.frutas;
+                return frutasCopia.sort((a, b) => b.cantidad - a.cantidad);
+                //return this.frutas.sort( (a , b) => b.cantidad - a.cantidad  )
+            }
+        },
+        methods:{
+            ...mapMutations(['aumentar', 'reinicar'])
+        }
+    }
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    frutas: [
+      {nombre: 'Manzana', cantidad: 0},
+      {nombre: 'Pera', cantidad: 0},
+      {nombre: 'Naranja', cantidad: 0}
+    ]
+  },
+  mutations: {
+    aumentar(state, index){
+      state.frutas[index].cantidad ++
+    },
+    reinicar(state){
+      state.frutas.forEach(element => {
+        element.cantidad = 0
+      });
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
